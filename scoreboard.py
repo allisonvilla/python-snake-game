e04f4f41b734f41c5f655bd348c66988c156559b
Allison <allison.villa@outlook.com>
2023-07-26 23:58:41 -0400

    added current high score in addition to all time high score

--- scoreboard.py
+++ scoreboard.py
@@ -8,8 +8,9 @@ COLOR = "white"
 class Scoreboard(Turtle):
     def __init__(self):
         self.score = 0
+        self.current_high_score = 0
         with open("data.txt") as data:
-            self.high_score = int(data.read())
+            self.all_time_high_score = int(data.read())
         super().__init__()
         self.color(COLOR)
         self.hideturtle()
@@ -19,7 +20,7 @@ class Scoreboard(Turtle):
         self.display_score()
 
     def display_score(self):
-        self.write(f"🐍 Score: {self.score} 🐍 High Score: {self.high_score} 🐍", move=False, align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score}  |  High Score: {self.current_high_score}  |  All Time High Score: {self.all_time_high_score}", move=False, align=ALIGNMENT, font=FONT)
 
     def update_score(self):
         self.clear()
@@ -27,10 +28,12 @@ class Scoreboard(Turtle):
         self.display_score()
 
     def reset_game(self):
-        if self.score > self.high_score:
-            self.high_score = self.score
+        if self.score > self.current_high_score:
+            self.current_high_score = self.score
+        if self.score > self.all_time_high_score:
+            self.all_time_high_score = self.score
             with open("data.txt", mode="w") as data:
-                data.write(f"{self.high_score}")
+                data.write(f"{self.all_time_high_score}")
         self.score = 0
         self.update_score()
 
